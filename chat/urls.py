46bf7970ff240a588a5d25c8d7ca71eaed32a452
BUAAwei <112938142+BUAAwei@users.noreply.github.com>
2023-08-27 16:47:54 +0800

    添加存储信息函数

--- chat/urls.py
+++ chat/urls.py
@@ -4,4 +4,5 @@ from .views import *
 urlpatterns = [
     path('get_team_members_and_permissions', get_team_members_and_permissions, name='get_team_members_and_permissions'),
     path('get_teams_for_user', get_teams_for_user, name='get_teams_for_user'),
+    path('store_message', store_message, name='store_message'),
 ]
