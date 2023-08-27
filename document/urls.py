a9a6327c75a5640bf2d0e9968957a0e8de11dc4a
caryz <Zhangkairui250@126.com>
2023-08-27 23:06:01 +0800

    document

--- document/urls.py
+++ document/urls.py
@@ -4,4 +4,7 @@ from .views import *
 urlpatterns = [
     path('upload_file', upload_file, name='upload_file'),
     path('download_file', download_file, name='download_file'),
+    path('upload_saved_document', upload_saved_document, name='upload_saved_document'),
+    path('create_document', create_document, name='create_document'),
+    path('download_saved_document', download_saved_document, name='download_saved_document'),
 ]
--- document/views.py
+++ document/views.py
@@ -4,7 +4,7 @@ from team.models import *
 from message.models import *
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
-from .models import File
+from .models import File, Document, SavedDocument
 from django.http import HttpResponse
 
 
@@ -27,3 +27,41 @@ def download_file(request):
     response['Content-Disposition'] = f'attachment; filename="{file_obj.file_content.name}"'
 
     return response
+
+
+@csrf_exempt
+@require_http_methods(['POST'])
+def upload_saved_document(request):
+    data_json = json.loads(request.body)
+    document_id = data_json.get('document_id')
+    document = Document.objects.get(document_id=document_id)
+    uploaded_file = request.FILES['file']
+    savedDocument = SavedDocument.objects.create(sd_document=document, sd_file=uploaded_file)
+    document.document_saves.add(savedDocument)
+
+    return JsonResponse({'errno': 0, 'message': 'File uploaded successfully.'})
+
+
+@csrf_exempt
+@require_http_methods(['POST'])
+def create_document(request):
+    data_json = json.loads(request.body)
+    document_name = data_json.get('document_name')
+    team_id = data_json.get('team_id')
+    team = Team.objects.get(team_id=team_id)
+    document = Document.objects.create(document_name=document_name, document_team=team)
+
+    return JsonResponse({'errno': 0, 'message': 'File uploaded successfully.', 'document_id': document.document_id})
+
+
+@csrf_exempt
+@require_http_methods(['POST'])
+def download_saved_document(request):
+    data = json.loads(request.body)
+    document_id = data.get('document_id')
+    document = Document.objects.get(document_id=document_id)
+    recent_save = document.document_saves.last()
+    response = HttpResponse(recent_save.sd_file.read(), content_type='application/octet-stream')
+    response['Content-Disposition'] = f'attachment; filename="{recent_save.sd_file.name}"'
+
+    return JsonResponse({'errno': 0, 'message': 'File uploaded successfully.', 'document_id': document.document_id})
