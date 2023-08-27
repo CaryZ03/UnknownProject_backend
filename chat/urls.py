7ac23a41ed19f6b166e6c520a2e40c43de30e99e
BUAAwei <112938142+BUAAwei@users.noreply.github.com>
2023-08-27 11:31:34 +0800

    qqq

--- chat/urls.py
+++ chat/urls.py
@@ -3,4 +3,5 @@ from .views import *
 
 urlpatterns = [
     path('get_team_members_and_permissions', get_team_members_and_permissions, name='get_team_members_and_permissions'),
+    path('get_teams_for_user', get_teams_for_user, name='get_teams_for_user'),
 ]
--- chat/views.py
+++ chat/views.py
@@ -15,6 +15,7 @@ from django.core.files.base import ContentFile
 from user.views import login_required, not_login_required
 
 
+@csrf_exempt
 @require_http_methods(['POST'])
 def get_team_members_and_permissions(request):
     data = json.loads(request.body)
@@ -22,24 +23,44 @@ def get_team_members_and_permissions(request):
     tm_user_id = data.get('tm_user_id')
 
     team = Team.objects.get(team_id=team_id)
-    user = TeamMember.objects.get(team_id=team_id, tm_user_id=tm_user_id)
+    user = User.objects.get(user_id=tm_user_id)
+    tm_user = TeamMember.objects.get(tm_team_id=team, tm_user_id=user)
 
     members_info = []
     is_creator_or_manager = False
 
     for member in team.team_member.all():
         member_info = {
-            'tm_user_id': member.tm_user_id,
+            'tm_user_id': member.tm_user_id.user_id,
             'tm_user_nickname': member.tm_user_nickname,
         }
         members_info.append(member_info)
 
-    if user.permission_choices in ['creator', 'manager']:
+    if tm_user.tm_user_permissions in ['creator', 'manager']:
         is_creator_or_manager = True
 
     response_data = {
         'members': members_info,
-        'is_creator_or_manager': is_creator_or_manager,
+        'is_creator_or_manager': is_creator_or_manager
     }
 
     return JsonResponse(response_data)
+
+
+@csrf_exempt
+@require_http_methods(['POST'])
+def get_teams_for_user(request):
+    data = json.loads(request.body)
+    tm_user_id = data.get('tm_user_id')
+    user = User.objects.get(user_id=tm_user_id)
+
+    teams_info = []
+
+    for team_member in TeamMember.objects.filter(tm_user_id=user):
+        team_info = {
+            'team_id': team_member.tm_team_id.team_id
+        }
+        teams_info.append(team_info)
+
+    return JsonResponse({'teams': teams_info})
+
