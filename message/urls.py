18e7164a414f64f0450fcaed48216c61f3a46e8d
BUAAwei <112938142+BUAAwei@users.noreply.github.com>
2023-08-28 10:17:56 +0800

    高桑的要求

--- message/urls.py
+++ message/urls.py
@@ -4,5 +4,5 @@ from .views import *
 urlpatterns = [
     path('group_send_notification_to_user', group_send_notification_to_user, name='group_send_notification_to_user'),
     path('check_notification_list', check_notification_list, name='check_notification_list'),
-
+    path('post_skip_info', post_skip_info, name='post_skip_info'),
 ]
