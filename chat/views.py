ad33f85d505a67ca3657c3f2be68bbeca0d7aed9
BUAAwei <112938142+BUAAwei@users.noreply.github.com>
2023-08-30 20:32:24 +0800

    高老师想查找团队

--- chat/views.py
+++ chat/views.py
@@ -201,3 +201,25 @@ def create_private_chat(request):
     new_private_chat.save()
 
     return JsonResponse({'msg': "创建私聊成功"})
+
+
+@csrf_exempt
+@require_http_methods(['POST'])
+def acquire_private_chat(request):
+    data = json.loads(request.body)
+    user_id = data.get('user_id')
+    user = User.objects.get(user_id=user_id)
+    private_chats_info = []
+    for team_member in TeamMember.objects.filter(tm_user_id=user):
+        private_chats = PrivateChat.objects.filter(pc_members=team_member)
+        for pc in private_chats:
+            pc_members = pc.pc_members.all()
+            for pc_member in pc_members:
+                if pc_member != team_member:
+                    private_chat_info = {
+                        'pc_id': pc.pc_id,
+                        'opposite_name': pc_member.tm_user_id.user_name
+                    }
+                    private_chats_info.append(private_chat_info)
+
+    return JsonResponse({'private_chats_info': private_chats_info})
